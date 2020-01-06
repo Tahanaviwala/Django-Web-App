f46ebc09578cbd5ac193a32d883c0ecd936606ac
Taha naviwala <48213543+Tahanaviwala@users.noreply.github.com>
2020-01-06 19:46:05 +0500

    use html form to get and display added numbers

--- project_1/app_1/views.py
+++ project_1/app_1/views.py
@@ -4,4 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def app(request):
-    return HttpResponse("<h1>Hello World</h1>")
+ #   return HttpResponse("<h1>Hello World</h1>")
+ #   return render(request , "index.html")
+ #   a = input("Enter Your Name")
+     return render(request , "home.html" , {"name" : "Taha Naviwala"})
+
+
+def con(request):
+#   b = input("Enter Contact No")
+    return render(request , "contact.html" , {"contact" : "03302843162"})
+
+
+def add(request):
+    val1 = request.GET["num1"]
+    val2 = request.GET["num2"]
+    total  = val1 + val2
+    return render(request , "result.html" , {"result":total})
